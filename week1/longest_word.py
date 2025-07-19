627f6414015d8a825078c4249b1c497f5088c69d
pratyanja-k <pratyanja@ai4mtech.com>
2025-07-19 14:41:02 +0530

    created function for longest word

--- week1/longest_word.py
+++ week1/longest_word.py
@@ -1,11 +1,18 @@
 sentence = "I code because I love solving111 problems"
- 
-words = sentence.split()
 
-longest = ""
 
-for word in words:
-    if len(word)> len(longest):
-        longest = word
 
-print(longest)
+
+def longest_word(sentence):
+    words = sentence.split()
+
+    longest = ""
+
+    for word in words:
+        if len(word)> len(longest):
+            longest = word
+
+    return longest
+
+long_word = longest_word(sentence)
+print(long_word)
